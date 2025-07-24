5c8d07df08638b4d1e7c4be8e1096c7ce1395d19
Vimal Boopathi D <boopathivim17@gmail.com>
2025-07-25 00:03:40 +0530

    CategoryShowCase

---
+++ src/components/Ratings.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Ratings = () => {
+  return (
+     <span className='ratings'>
+        <i className="icofont-ui-rating"></i>
+        <i className="icofont-ui-rating"></i>
+        <i className="icofont-ui-rating"></i>
+        <i className="icofont-ui-rating"></i>
+        <i className="icofont-ui-rating"></i>
+    </span>
+  )
+}
+
+export default Ratings
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Banner from './Banner'
 import HomeCategory from './HomeCategory'
+import CategoryShowCase from './CategoryShowCase'
 
 const Home = () => {
   return (
     <div>
       <Banner/>
       <HomeCategory/>
+      <CategoryShowCase/>
     </div>
   )
 }
