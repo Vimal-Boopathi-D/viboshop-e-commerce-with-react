be81f7fc1c18533a7082384711c5e73bc0d70b4d
Vimal Boopathi D <boopathivim17@gmail.com>
2025-07-26 23:26:49 +0530

    deployment resolved

--- src/home/AppSection.jsx
+++ src/home/AppSection.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import img01 from "../assets/images/app/01.jpg";
+import img02 from "../assets/images/app/02.jpg";
 import { Link } from "react-router-dom";
 const btnText = "Sign up for free";
 const title = "Shop Anytime, Anywhere";
@@ -20,12 +22,12 @@ const AppSection = () => {
           <ul className="lab-ul">
             <li>
               <a href="#">
-                <img src="/src/assets/images/app/01.jpg" alt="" />
+                <img src={img01} alt="" />
               </a>
             </li>
             <li>
               <a href="#">
-                <img src="/src/assets/images/app/02.jpg" alt="" />
+                <img src={img02} alt="" />
               </a>
             </li>
           </ul>
